29f20fb9d0ee0c2c3841a4365f7788bed47914fb
IramML <irammartinezlamas@gmail.com>
2022-11-06 23:55:04 -0600

    Set public access to repository methods

--- Data/Repository/ComicsRepository.swift
+++ Data/Repository/ComicsRepository.swift
@@ -8,32 +8,32 @@
 import Foundation
 import Domain
 
-final class ComicsRepository {
+public final class ComicsRepository {
     private let comicsRemoteDataSource: ComicsRemoteDataSource
     private let comicsLocalDataSource: ComicsLocalDataSource
     
-    init(comicsRemoteDataSource: ComicsRemoteDataSource, comicsLocalDataSource: ComicsLocalDataSource) {
+    public init(comicsRemoteDataSource: ComicsRemoteDataSource, comicsLocalDataSource: ComicsLocalDataSource) {
         self.comicsRemoteDataSource = comicsRemoteDataSource
         self.comicsLocalDataSource = comicsLocalDataSource
     }
     
-    func getComics(completion: @escaping (RemoteResult<[Comic]>) -> Void) {
+    public func getComics(completion: @escaping (RemoteResult<[Comic]>) -> Void) {
         self.comicsRemoteDataSource.getComics(completion: completion)
     }
     
-    func getFavoriteComics(completion: @escaping (RemoteResult<[Comic]>) -> Void) {
+    public func getFavoriteComics(completion: @escaping (RemoteResult<[Comic]>) -> Void) {
         comicsLocalDataSource.getFavoriteComics(completion: completion)
     }
     
-    func getFavoriteComic(byId comicId: Int, completion: @escaping (RemoteResult<Comic?>) -> Void) {
+    public func getFavoriteComic(byId comicId: Int, completion: @escaping (RemoteResult<Comic?>) -> Void) {
         comicsLocalDataSource.getFavoriteComic(byId: comicId, completion: completion)
     }
     
-    func addFavoriteComic(comic: Comic, completion: @escaping (RemoteResult<Bool>) -> Void) {
+    public func addFavoriteComic(comic: Comic, completion: @escaping (RemoteResult<Bool>) -> Void) {
         comicsLocalDataSource.addFavoriteComic(comic, completion: completion)
     }
     
-    func removeFavoriteComic(byId comicId: Int, completion: @escaping (RemoteResult<Bool>) -> Void) {
+    public func removeFavoriteComic(byId comicId: Int, completion: @escaping (RemoteResult<Bool>) -> Void) {
         comicsLocalDataSource.removeFavoriteComic(byId: comicId, completion: completion)
     }
 }
